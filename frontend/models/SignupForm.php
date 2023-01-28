76c489d4db99736a22701d425175bca4e2b5e421
bot5294 <5294aqib@gmail.com>
2023-01-28 11:14:30 +0530

    Added Firstname and Lastname

--- frontend/models/SignupForm.php
+++ frontend/models/SignupForm.php
@@ -12,6 +12,8 @@ use common\models\User;
 class SignupForm extends Model
 {
     public $username;
+    public $firstname;
+    public $lastname;
     public $email;
     public $password;
 
@@ -22,6 +24,12 @@ class SignupForm extends Model
     public function rules()
     {
         return [
+            ['firstname','required'],
+            ['firstname','string','max'=>255],
+
+            ['lastname','required'],
+            ['lastname','string','max'=>255],
+
             ['username', 'trim'],
             ['username', 'required'],
             ['username', 'unique', 'targetClass' => '\common\models\User', 'message' => 'This username has already been taken.'],
@@ -50,6 +58,8 @@ class SignupForm extends Model
         }
         
         $user = new User();
+        $user->firstname = $this->firstname;
+        $user->lastname = $this->lastname;
         $user->username = $this->username;
         $user->email = $this->email;
         $user->setPassword($this->password);
