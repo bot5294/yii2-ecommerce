53d910bb2f30f2376011714aef75026d31ef03a9
bot5294 <5294aqib@gmail.com>
2023-02-07 11:21:05 +0530

    send mail implemented (cartcontroller in next commit)

--- common/mail/layouts/html.php
+++ common/mail/layouts/html.php
@@ -13,12 +13,14 @@ use yii\helpers\Html;
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=<?= Yii::$app->charset ?>" />
     <title><?= Html::encode($this->title) ?></title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <?php $this->head() ?>
 </head>
 <body>
     <?php $this->beginBody() ?>
     <?= $content ?>
     <?php $this->endBody() ?>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 </body>
 </html>
 <?php $this->endPage();
